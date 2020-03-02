b67374aef8fbce3b729fb5b39c543f9df7b37f1d
JulioLP <julio.piva@present-technologies.com>
2020-03-02 14:52:59 +0000

    Implement the webcam canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useRef, useLayoutEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  
+  useLayoutEffect(() => {
+    if (videoRef && videoRef.current) {
+      startCamera(videoRef);
+    }
+  })
+
+
 	return (
 		<div>
-			<div className="Title"> This is your Webcam ! </div>
       <div className="Container">
-        <video id="video" width="720" height="560" autoPlay muted />
+			  <div> This is your Webcam ! </div>
+        <video ref={videoRef} id="video" width="720" height="560" autoPlay muted />
       </div>
 		</div>
 	);
+}
+
+
+const startCamera = (videoRef:React.RefObject<HTMLVideoElement>) => {
+  navigator.getUserMedia(
+    { video: {} },
+    stream => {
+      if (videoRef.current) {
+        videoRef.current.srcObject = stream;
+      }
+    },
+    err => console.error(err)
+  )
 }
 
 export default App;
