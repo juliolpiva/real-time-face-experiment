b782ee8d545d46f4735b8c2bef8c33c83cc62aab
JulioLP <julio.piva@present-technologies.com>
2020-04-26 18:18:24 +0100

    feat: Create a theme colors file

---
+++ src/theme/colors.tsx
@@ -0,0 +1,17 @@
+export const black = '#000';
+export const white = '#fff';
+
+export const lighterGrey = '#fafafa';
+export const lightGrey = '#f2f2f2';
+export const lightGrey2 = '#e5e5e5';
+export const grey = '#cccccc';
+export const silverGrey = '#c2c2c2';
+export const warmGrey = '#999999';
+export const darkGrey = '#666666';
+export const veryLightGrey = '#dddddd';
+export const nearBlack = '#333333';
+
+export const successGreen = '#0a9408';
+export const errorRed = '#ff6b6b';
+export const alertOrange = '#fe5937';
+export const disabled = `${warmGrey}`;
